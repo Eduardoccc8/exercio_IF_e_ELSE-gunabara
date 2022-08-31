72f7ed48b9fa92c19f550d7d647f2e535c9cc020
Cassio Eduardo da Silva <cassioeduardoccc8@gmail.com>
2022-08-31 12:29:02 -0300

    exercicios 11 if e else

---
+++ ex008.js
@@ -0,0 +1,10 @@
+var vel = 61
+console.log (`A velocida do seu carro e ${vel}km/hr`)
+if (vel > 60){
+    console.log(`Voce ultrapassou a velocidade permitida!! voce será multado!!`)
+}
+
+
+
+console.log(`dirija sempre usando cinto de segurança!!`)
+
